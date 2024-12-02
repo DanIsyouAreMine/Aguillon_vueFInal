8c7cac7a699fad65567f890252f84d7ac8129d2e
DanIsyouAreMine <danchristianaguillon9@gmail.com>
2024-12-02 11:12:51 +0800

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,23 @@
-// src/router.js
 import { createRouter, createWebHistory } from 'vue-router'
-// Import HomeView directly since it will be the main view
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // Home page, where all the dogs will be listed
     {
       path: '/',
       name: 'home',
       component: HomeView,
     },
-    // Add Dog page, where users can add new dogs for adoption
     {
       path: '/add',
       name: 'add-dog',
-      component: () => import('../views/AddDog.vue'), // Lazy-loaded
+      component: () => import('../views/AddDog.vue'),
     },
-    // Edit Dog page, to edit an existing dog's details
     {
-      path: '/edit/:id', // :id is a dynamic route parameter
+      path: '/edit/:id', //
       name: 'edit-dog',
-      component: () => import('../views/EditDog.vue'), // Lazy-loaded
+      component: () => import('../views/EditDog.vue'),
     },
   ],
 })
